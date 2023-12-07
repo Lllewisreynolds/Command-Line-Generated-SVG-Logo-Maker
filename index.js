8df41dd0444a9bad431a1a255e642f7734ccbc15
Lllewisreynolds <lsreynolds108@gmail.com>
2023-12-07 13:03:27 +0000

    Three shape classes imported from the lib directory and inquirer and fs libraries loaded in index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// Individual shape classes brought in from ./lib/shapes module
+const { Triangle, Square, Circle } = require("./lib/shapes");
+
+// Inquirer & File System node libraries/modules made accessible via import
+const inquirer = require("inquirer");
+
+const fs = require("fs");
+
